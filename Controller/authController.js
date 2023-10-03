4e6bc7b3b9feae6ddef2efa53129a92c2ce30d6d
TajalTechnology <mrpresident1189@gmail.com>
2023-10-03 23:29:58 +0600

    ADD: otp

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,6 +1,12 @@
 const Company = require("../Model/companyModel");
 const Worker = require("../Model/workerModel");
 const jsonwebtoken = require('jsonwebtoken');
+const { Vonage } = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+    apiKey: "5d32fcef",
+    apiSecret: "7SolNHoyZmHtJZ2m"
+});
 
 // Index - Show All Data.
 const index = async (req, res) => {
@@ -18,35 +24,41 @@ const store = async (req, res) => {
     const query = {
         password: req.body.password,
         $or: [
-            { role:'admin' }, 
+            { role: 'admin' },
             { role: 'company' },
             { role: 'worker' }
         ]
-        
-     };
 
-     try {
-        if(req.body.email){
+    };
+
+    try {
+        if (req.body.email) {
             query.email = req.body.email;
-            const company = await Company.findOne(query); 
+            const company = await Company.findOne(query);
             if (!company) throw new ApiError('Does not have any company or admin with this credentials');
-            data.token = jsonwebtoken.sign({data: company}, 'JWT.secret');
+            data.token = jsonwebtoken.sign({ data: company }, 'JWT.secret');
             data.role = company.role;
-        }else if(req.body.phone){
+        } else if (req.body.phone) {
             query.phone = req.body.phone;
-            const worker = await Worker.findOne(query); 
+
+            const from = "Vonage APIs"
+            const to = "+8801785607926"
+            const text = 'A text message sent using the Vonage SMS API'
+            const send = await vonage.sms.send({ to, from, text })
+
+            const worker = await Worker.findOne(query);
             if (!worker) throw new ApiError('Does not have any worker with this credentials');
-            data.token = jsonwebtoken.sign({data: worker}, 'JWT.secret');
+            data.token = jsonwebtoken.sign({ data: worker }, 'JWT.secret');
             data.role = worker.role;
         }
-        if(data.token)data.sucess = true
+        if (data.token) data.sucess = true
         else throw new ApiError('Credentials mismatch')
-        return res.status(200).send({ data})
-     } catch (error) {
-        return res.status(400).send({ error: error.message})
-     }
+        return res.status(200).send({ data })
+    } catch (error) {
+        return res.status(400).send({ error: error.message })
+    }
 
- }
+}
 
 // update 
 const update = async (req, res) => { }
